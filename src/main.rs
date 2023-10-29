4fa684317e7160773d1f945eb0ab4caaba16b7fd
projectfinalaudio <irfanghat@gmail.com>
2023-10-29 18:54:53 +0300

    LeetCode-1: Two Sum -> Using literal iterator i.e 0..nums.len(), non-inclussive on the upper bound

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,27 @@ use std::io;
 
 fn main() -> () {
     let nums: Vec<i32> = vec![2, 7, 11, 15];
-    // let nums: Vec<i32> = vec![3,2,4];
-    // let nums: Vec<i32> = vec![3,3];
+    println!(r#"
+    Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    "#);
 
+    println!(r#"
+    Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    "#);
+
+    println!("Given the array: {:?}", nums);
+    /*
+    * !!! Test Cases !!!
+    * let nums: Vec<i32> = vec![3,2,4];
+    * let nums: Vec<i32> = vec![3,3];
+    *
+    */
     let mut target = String::new();
     println!("Please provide a target >");
     io::stdin().read_line(&mut target).unwrap();
